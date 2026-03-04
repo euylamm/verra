e4b691ca59ed5a54d3a5f57b8e4c46d689c7fc13
Alam <euylamm@gmail.com>
2026-03-04 13:32:43 +0700

    boilerplate

---
+++ playwright.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+    testDir: './tests',
+    timeout: 30_000,
+    retries: 1,
+    workers: 2,
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+
+    reporter: [
+        ['html', { outputFolder: 'evidences', open: 'never'}], ['list'],
+    ],
+
+    use: {
+        baseURL: 'https://www.automationexercise.com',
+        headless: true,
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        trace: 'on-first-retry',
+    },
+    
+    projects: [
+        {
+            name: 'store-ui',
+            testDir: './tests/store/e2e',
+            use: { ...devices['Desktop Chrome']},
+        },
+        {
+            name: 'store-api',
+            testDir: './tests/store/api'
+        },
+    ],
+
+});
